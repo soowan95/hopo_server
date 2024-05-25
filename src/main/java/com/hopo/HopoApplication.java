43d3bb4871989dedefde9d2067176b16ce4aedd0
soowan95 <ksw9565@gmail.com>
2024-05-25 14:52:33 +0900

    240525 프로젝트 시작

---
+++ src/main/java/com/hopo/HopoApplication.java
@@ -0,0 +1,13 @@
+package com.hopo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hopo/HopoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
