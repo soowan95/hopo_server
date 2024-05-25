1015db1765d74e282d969a11f384d07d2dd2be68
soowan95 <ksw9565@gmail.com>
2024-05-25 16:41:46 +0900

    [feat] swagger-ui, spring security, jwt 초기 설정

---
+++ src/main/java/com/hopo/config/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.hopo.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.hopo.config.jwt.JwtAuthenticationEntryPoint;
+import com.hopo.config.jwt.JwtFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity // Spring Security 활성화
+@EnableMethodSecurity // @preAutorize 어노테이션 메소드 단위로 추가
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private static final String[] PERMIT_URL_ARRAY = {
+		"/v3/api-docs/**",
+		"/swagger-ui/**",
+		"/swagger-resources/**",
+		"/swagger-resources",
+		"/hopo-ui.html",
+		"/webjars/**",
+		"/"
+	};
+
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final JwtFilter jwtFilter;
+
+	// 비밀번호 암호화
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// cors 설정
+	CorsConfigurationSource corsConfigurationSource() {
+		return request -> {
+			CorsConfiguration config = new CorsConfiguration();
+			config.setAllowedHeaders(Collections.singletonList("*"));
+			config.setAllowedMethods(Collections.singletonList("*"));
+
+			config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000"));
+			config.setAllowCredentials(true);
+
+			return config;
+		};
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http
+			.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+			.csrf(AbstractHttpConfigurer::disable)
+			.exceptionHandling(
+				(exceptionConfig) -> exceptionConfig.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+			.sessionManagement((sessionConfig) -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+				.requestMatchers(PERMIT_URL_ARRAY).permitAll()
+				.anyRequest().authenticated());
+
+		return http.build();
+	}
+}
