bdc456e854ccc563fd346cb1aa26a67f41d4e46b
soowan95 <ksw9565@gmail.com>
2025-01-18 12:41:16 +0900

    fix(test): import 수정 - HopoServiceTest 의 import 오류 수정 #0

--- src/test/java/com/hopo/_global/service/HopoServiceTest.java
+++ src/test/java/com/hopo/_global/service/HopoServiceTest.java
@@ -3,6 +3,7 @@ package com.hopo._global.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -10,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Spy;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
