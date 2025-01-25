cbe58ae4936f8b20d676fe05824607f4919d685c
soowan95 <ksw9565@gmail.com>
2025-01-25 11:54:32 +0900

    test(controller): HopoControllerTest - HopoController 의 test 코드 수정 #0

--- src/test/java/com/hopo/_global/controller/HopoControllerTest.java
+++ src/test/java/com/hopo/_global/controller/HopoControllerTest.java
@@ -7,14 +7,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -34,14 +32,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @ActiveProfiles("p1")
-@WebMvcTest(HopoController.class)
-@ContextConfiguration(classes = HopoController.class)
+@SpringBootTest
 @ControllerTest
-@ComponentScan(basePackages = "com.hopo._global.controller")
-@Import(TestConfig.class)
+@Import({TestConfig.class, DtoRegistry.class})
+@AutoConfigureMockMvc
 public class HopoControllerTest {
 
-	@SpyBean
+	@Autowired
 	private DtoRegistry dtoRegistry;
 
 	@Autowired
