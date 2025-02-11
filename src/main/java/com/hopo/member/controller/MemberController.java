264123025e78856e2da6da0d5340ef1180e9dc1a
soowan95 <ksw9565@gmail.com>
2025-02-11 23:03:54 +0900

    fix(controller): 생성자 주입방식으로 변경 #25

--- src/main/java/com/hopo/member/controller/MemberController.java
+++ src/main/java/com/hopo/member/controller/MemberController.java
@@ -6,20 +6,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hopo._config.registry.DtoRegistry;
+import com.hopo._config.registry.ServiceRegistry;
+import com.hopo._global.controller.HopoController;
 import com.hopo.member.dto.request.SignUpRequest;
 import com.hopo.member.dto.response.MemberResponse;
 import com.hopo.member.service.MemberServiceImpl;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/member")
 @Tag(name = "사용자", description = "사용자 API 문서")
-@RequiredArgsConstructor
-public class MemberController {
+public class MemberController extends HopoController {
 
 	private final MemberServiceImpl memberService;
+
+	public MemberController(ServiceRegistry serviceRegistry,
+		DtoRegistry dtoRegistry, MemberServiceImpl memberService) {
+		super(serviceRegistry, dtoRegistry);
+		this.memberService = memberService;
+	}
 
 	@PostMapping("/sign_up")
 	public ResponseEntity<MemberResponse> signUp(@RequestBody SignUpRequest request) {
